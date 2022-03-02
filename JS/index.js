0a480a8708b8711952feba0a89f23af886c9cd10
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-03-02 15:43:30 +0300

    st:file-structure - initialize files

---
+++ JS/index.js
@@ -0,0 +1,6 @@
+var add = function(number1, number2) {
+    return number1 + number2;
+  };
+  var number1 = prompt("Enter First number:");
+  var number2 = prompt("Enter Second number:");
+  alert(add(number1, number2));
