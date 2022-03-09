a8d6012fd99abf741d8e17df3459596108c94d58
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-03-09 11:57:17 +0300

    st:form - add functions to take user input

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,28 @@
-var add = function(number1, number2) {
+// let add = function(number1, number2) {
+//     return number1 + number2;
+//   };
+//   let number1 = prompt("Enter First number:");
+//   let number2 = prompt("Enter Second number:");
+//   alert(add(number1, number2));
+
+// Calculations done by the following.
+  let add = function(number1, number2) {
     return number1 + number2;
   };
-  var number1 = prompt("Enter First number:");
-  var number2 = prompt("Enter Second number:");
-  alert(add(number1, number2));
+  let subtract = function(number1, number2) {
+    return number1 - number2;
+  };
+  let multiply = function(number1, number2) {
+    return number1 * number2;
+  };
+  let divide = function(number1, number2) {
+    return number1 / number2;
+  };
+
+// gathering user input
+$(document).ready(function(){
+  let number1 = prompt("Enter First number:");
+  let number2 = prompt("Enter Second number:");
+   alert(add(number1, number2));
+
+});
