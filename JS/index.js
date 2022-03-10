34aba1db1c9b130d51c850b275b20b78682e780b
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-03-10 08:59:18 +0300

    st:form - multiplication, subtraction and division

--- JS/index.js
+++ JS/index.js
@@ -5,7 +5,7 @@
 //   let number2 = prompt("Enter Second number:");
 //   alert(add(number1, number2));
 
-// Calculations done by the following.
+// Calculations done by the following.# backend logic
   let add = function(number1, number2) {
     return number1 + number2;
   };
@@ -19,14 +19,8 @@
     return number1 / number2;
   };
 
-// gathering user input
-// $(document).ready(function(){
-//   let number1 = prompt("Enter First number:");
-//   let number2 = prompt("Enter Second number:");
-//    alert(add(number1, number2));
-
-// });
-
+  
+// front-end logic
 $(document).ready(function() {
   $("form#add").submit(function(event) {
     event.preventDefault();
@@ -35,5 +29,15 @@ $(document).ready(function() {
 
   let addResult = add(number1, number2);
 $("#output").text(addResult);
+  });
+
+  // subraction
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+  let number1 = parseInt($("#subtract1").val());
+  let number2 = parseInt($("#subtract2").val());
+
+  let subtractResult = subtract(number1, number2);
+$("#output").text(subtractResult);
   });
 });
